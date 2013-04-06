251912330d56b8203ba9417d6588d42451261178
Samantha Geitz <samanthamichele7@gmail.com>
2013-04-06 14:22:44 -0500

    Finished homepage with full-screen photo and text

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,10 @@
 Portfolio::Application.routes.draw do
+
+get 'pages/home' => 'high_voltage/pages#show', :id => 'home'
+get 'pages/about' => 'high_voltage/pages#show', :id => 'about'
+get 'pages/contact' => 'high_voltage/pages#show', :id => 'contact'
+get 'pages/portfolio' => 'high_voltage/pages#show', :id => 'portfolio'
+get 'pages/resume' => 'high_voltage/pages#show', :id => 'resume'
+
+root :to => 'high_voltage/pages#show', :id => 'home'
 end
