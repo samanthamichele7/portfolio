493c997f0ae5764f305d93bc32db379e87764766
Samantha Geitz <samanthamichele7@gmail.com>
2013-04-09 01:04:00 -0500

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Portfolio::Application.routes.draw do
 
-get 'pages/home' => 'high_voltage/pages#show', :id => 'home'
-get 'pages/about' => 'high_voltage/pages#show', :id => 'about'
-get 'pages/contact' => 'high_voltage/pages#show', :id => 'contact'
-get 'pages/portfolio' => 'high_voltage/pages#show', :id => 'portfolio'
-get 'pages/resume' => 'high_voltage/pages#show', :id => 'resume'
+get 'home' => 'high_voltage/pages#show', :id => 'home'
+get 'about' => 'high_voltage/pages#show', :id => 'about'
+get 'contact' => 'high_voltage/pages#show', :id => 'contact'
+get 'portfolio' => 'high_voltage/pages#show', :id => 'portfolio'
+get 'resume' => 'high_voltage/pages#show', :id => 'resume'
 
 root :to => 'high_voltage/pages#show', :id => 'home'
 end
