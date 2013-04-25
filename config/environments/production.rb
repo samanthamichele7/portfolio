24f2a5b7eb874f4f3bd7b6a729cde84942b60532
Samantha Geitz <samanthamichele7@gmail.com>
2013-04-25 17:05:58 -0500

    Removed public assets trying to fix CDN issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Portfolio::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
     config.action_controller.asset_host = "//#{ENV['samanthageitzportfolio-assets']}.s3.amazonaws.com"
-
+  
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
