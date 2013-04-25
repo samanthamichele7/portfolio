98b0a062d531e9e346b9517f8d00f9eccd7ec387
Samantha Geitz <samanthamichele7@gmail.com>
2013-04-25 18:29:51 -0500

    I broke asset_sync :(  Starting over with a previous commit.

--- config/application.rb
+++ config/application.rb
@@ -69,7 +69,6 @@ module Portfolio
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.digest = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,9 +43,8 @@ Portfolio::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-    config.action_controller.asset_host =  "//#{ENV['samanthageitzportfolio-assets']}.s3.amazonaws.com"
-    config.action_mailer.asset_host = "http://#{ENV['samanthageitzportfolio-assets']}.s3.amazonaws.com"
-  
+  # config.action_controller.asset_host = "http://assets.example.com"
+
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
