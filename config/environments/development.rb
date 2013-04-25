bd2798c35e6697005e1322bea261ec32d94e2791
Samantha Geitz <samanthamichele7@gmail.com>
2013-04-25 14:53:29 -0500

    Improved responsive design

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,6 @@ Portfolio::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.serve_static_assets = false
 end
